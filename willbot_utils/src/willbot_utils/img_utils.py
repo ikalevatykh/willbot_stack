cce62aaa94c1d6bc4490b7954d4ebff54316ea95
Igor Kalevatykh <kalevatykh.work@gmail.com>
2019-01-21 00:27:58 +0100

    add new data types to image encoding

--- willbot_utils/src/willbot_utils/img_utils.py
+++ willbot_utils/src/willbot_utils/img_utils.py
@@ -15,11 +15,15 @@ def imgmsg_to_array(msg, encoding=None):
 
         return img_array
 
-    if msg.encoding == '32FC1':
-        img_array = np.frombuffer(msg.data, dtype=np.float32)
-        img_array = img_array.reshape((msg.height, msg.width, 1))
+    match = parse_encoding(msg.encoding)
+    if match:
+        img_array = np.frombuffer(msg.data, dtype=match[0])
+        img_array = img_array.reshape((msg.height, msg.width, match[1]))
 
-        if encoding is not None and encoding != '32FC1':
+        if encoding is not None:
+            match_to = parse_encoding(encoding)
+            if match_to and match_to[1] == match[1]:
+                return img_array.astype(match_to[0])
             raise Exception('Cannot convert {} image to {}'.format(
                 msg.encoding, encoding))
 
@@ -28,8 +32,20 @@ def imgmsg_to_array(msg, encoding=None):
     raise Exception('Cannot convert {} image'.format(msg.encoding))
 
 
+def parse_encoding(encoding):
+    if encoding.startswith('32F'):
+        dtype = np.float32
+    elif encoding.startswith('16U'):
+        dtype = np.uint16
+    elif encoding.startswith('16S'):
+        dtype = np.int16
+    else:
+        return None
+    return dtype, int(encoding[-1])
+
+
 def scale_depth(depth, near, far):
     depth = depth.clip(near, far)
     depth[np.isnan(depth)] = far
     depth = (255 * (depth - near) / (far - near)).astype(np.uint8)
-    return depth    
+    return depth
--- willbot_utils/src/willbot_utils/kinect.py
+++ willbot_utils/src/willbot_utils/kinect.py
@@ -16,13 +16,12 @@ class KinectListener():
     Useful when you capture data frequently.
     """
 
-    def __init__(self, color_topic='', depth_topic='', version=1):
+    def __init__(self, color_topic='', depth_topic=''):
         # TODO: From Kinect v2.0 can receive synchronized color and depth
         self._color_listener = ImageListener(
             color_topic or DEFAULT_COLOR_TOPIC)
         self._depth_listener = ImageListener(
             depth_topic or DEFAULT_COLOR_TOPIC)
-        self._depth_to_meter = 0.001 if version == 2 else 1.0
 
     def rgb(self):
         """ Return color image as np.uint8 array """
@@ -30,7 +29,10 @@ class KinectListener():
 
     def depth(self):
         """ Return depth data in meters as np.float32 array """
-        return self._depth_listener.latest() * self._depth_to_meter
+        depth = self._depth_listener.latest()
+        if depth.dtype == np.uint16:
+            return depth * np.float32(0.001)
+        return depth
 
     def depth_u8(self, near, far):
         """ Scale specified depth range to uint and return as np.uint8 array """
@@ -44,12 +46,11 @@ class KinectSnapshot():
     Useful when you capture data rarely.
     """
 
-    def __init__(self, color_topic='', depth_topic='', version=1):
+    def __init__(self, color_topic='', depth_topic=''):
         self._color_snapshot = ImageSnapshot(
             color_topic or DEFAULT_COLOR_TOPIC)
         self._depth_snapshot = ImageSnapshot(
             depth_topic or DEFAULT_DEPTH_TOPIC)
-        self._depth_to_meter = 0.001 if version == 2 else 1.0
 
     def rgb(self):
         """ Return color image as np.uint8 array """
@@ -57,7 +58,10 @@ class KinectSnapshot():
 
     def depth(self):
         """ Return depth data in meters as np.float32 array """
-        return self._depth_snapshot.wait_for_image(timeout=5) * self._depth_to_meter
+        depth = self._depth_snapshot.wait_for_image(timeout=5)
+        if depth.dtype == np.uint16:
+            return depth * np.float32(0.001)
+        return depth
 
     def depth_u8(self, near, far):
         """ Scale specified depth range to uint and return as np.uint8 array """
