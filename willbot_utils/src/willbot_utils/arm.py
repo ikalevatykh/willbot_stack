ec6fc86b4fe038d0d31d18b3522a724fd54d0aa1
Igor Kalevatykh <kalevatykh@gmail.com>
2019-01-25 15:58:36 +0100

    add helper functions

--- willbot_utils/src/willbot_utils/arm.py
+++ willbot_utils/src/willbot_utils/arm.py
@@ -19,10 +19,26 @@ class UR5(moveit_commander.MoveGroupCommander):
         vec = pose.pose.position
         return [vec.x, vec.y, vec.z]
 
+    def joint_move(self, pos, orn=None, wait=True):
+        """Plan and execute motion in joint space."""
+        if orn is None:
+            orn = self.get_current_rpy()
+        self.set_pose_target(list(pos) + list(orn))
+        plan = self.plan()
+        if not plan.joint_trajectory.points:
+            return False
+        return self.execute(plan, wait)
+
+    def linear_move(self, pos=None, orn=None, wait=True):
+        """Plan and execute motion in cartesian space."""
+        plan = CartesianPlan(self)
+        plan.move(pos, orn)
+        return plan.execute(wait)
+
     def cartesian(self):
         """Returns interface for cartesian path planning."""
         return CartesianPlan(self)
 
     def pick_place(self, hand, object_name):
-        """Returns interface for simple pick&place planning."""        
+        """Returns interface for simple pick&place planning."""
         return TopPickPlacePlan(self, hand, object_name)
