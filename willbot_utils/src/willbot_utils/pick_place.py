fb042e1b5e70dff59c03d7b78a54877710c1378d
Igor Kalevatykh <kalevatykh@gmail.com>
2019-02-04 20:19:57 +0100

    improve pick&place operation. Place a little bit above support plane.

--- willbot_utils/src/willbot_utils/pick_place.py
+++ willbot_utils/src/willbot_utils/pick_place.py
@@ -1,17 +1,20 @@
 import rospy
 
+
 class TopPickPlacePlan(object):
     """ Helper for planning a simple pick and place action """
 
-    def __init__(self, arm, hand, object_name=None):
+    def __init__(self, arm, hand, target_object=None):
         self._arm = arm
         self._hand = hand
-        self._object_name = object_name
+        self._object = target_object
+        self._object_width = 0
         self._pick_position = None
         self._place_position = None
 
-    def pick(self, pick_pos, pick_orn=None):
+    def pick(self, pick_pos, pick_orn=None, object_width=0):
         self._pick_position = (pick_pos, pick_orn)
+        self._object_width = object_width
 
     def place(self, place_pos, place_orn=None):
         self._place_position = (place_pos, place_orn)
@@ -27,18 +30,24 @@ class TopPickPlacePlan(object):
 
     def _pick(self, pick_pos, pick_orn=None):
         upper_pos = [pick_pos[0], pick_pos[1], pick_pos[2] + 0.1]
-        self._hand.open()
+
+        if not self._hand.open():
+            return False
+
         plan = self._arm.cartesian()
         plan.move(upper_pos, pick_orn)
         plan.move(pick_pos, pick_orn)
         if not plan.execute():
             return False
 
-        if self._object_name is not None:
-            self._arm.attach_object(
-                self._object_name, touch_links=self._hand.links)
-            rospy.rostime.wallsleep(0.1)
-        self._hand.close()
+        if self._object is not None:
+            self._object.attach(
+                link=self._arm.get_end_effector_link(),
+                touch_links=self._hand.links)
+
+        if not self._hand.parallel_grasp(self._object_width):
+            if self._object_width > 0:
+                return False
 
         plan = self._arm.cartesian()
         plan.move(upper_pos, pick_orn)
@@ -46,16 +55,19 @@ class TopPickPlacePlan(object):
 
     def _place(self, place_pos, place_orn=None):
         upper_pos = [place_pos[0], place_pos[1], place_pos[2] + 0.1]
+        above_pos = [place_pos[0], place_pos[1], place_pos[2] + 0.002]
         plan = self._arm.cartesian()
         plan.move(upper_pos, place_orn)
-        plan.move(place_pos, place_orn)
+        plan.move(above_pos, place_orn)
         if not plan.execute():
             return False
 
-        self._hand.open()
-        if self._object_name is not None:
-            self._arm.detach_object()
-            rospy.rostime.wallsleep(0.1)
+        if not self._hand.open():
+            return False
+
+        if self._object is not None:
+            self._object.detach()
+            self._object.shift((0, 0, -0.002))
 
         plan = self._arm.cartesian()
         plan.move(upper_pos, place_orn)
