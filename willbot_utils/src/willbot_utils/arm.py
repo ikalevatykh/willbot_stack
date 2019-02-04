cfa9d8b332acc0f085daaf58ac2024d7ed98f850
Igor Kalevatykh <kalevatykh@gmail.com>
2019-02-04 20:16:53 +0100

    arm holds gripper interface

--- willbot_utils/src/willbot_utils/arm.py
+++ willbot_utils/src/willbot_utils/arm.py
@@ -1,6 +1,10 @@
 import rospy
 import moveit_commander
 
+from moveit_msgs.msg import CollisionObject, AttachedCollisionObject
+from moveit_msgs.msg import PlanningScene
+from moveit_msgs.srv import ApplyPlanningScene
+
 from willbot_utils.cartesian import CartesianPlan
 from willbot_utils.pick_place import TopPickPlacePlan
 
@@ -8,10 +12,16 @@ from willbot_utils.pick_place import TopPickPlacePlan
 class UR5(moveit_commander.MoveGroupCommander):
     """A thin wrapper for MoveIt move group for willbot arm control."""
 
-    def __init__(self, group_name="manipulator"):
+    def __init__(self, group_name="manipulator", hand=None):
         super(UR5, self).__init__(group_name)
         # set default path planner
         self.set_planner_id("RRTConnectkConfigDefault")
+        self._hand = hand
+
+    @property
+    def hand(self):
+        """Gripper attached to the arm."""
+        return self._hand
 
     def get_current_position(self):
         """Returns a list of 3 elements defining the [x, y, z] of the end-effector."""
@@ -39,6 +49,6 @@ class UR5(moveit_commander.MoveGroupCommander):
         """Returns interface for cartesian path planning."""
         return CartesianPlan(self)
 
-    def pick_place(self, hand, object_name):
+    def pick_place(self, target_object):
         """Returns interface for simple pick&place planning."""
-        return TopPickPlacePlan(self, hand, object_name)
+        return TopPickPlacePlan(self, self._hand, target_object)
