ed85bd7052c5dd60bf6e22af6e45e6211114abe3
Igor Kalevatykh <kalevatykh@gmail.com>
2019-02-19 00:18:00 +0100

    default topics for kinect cameras

--- willbot_utils/src/willbot_utils/kinect.py
+++ willbot_utils/src/willbot_utils/kinect.py
@@ -66,3 +66,40 @@ class KinectSnapshot():
     def depth_u8(self, near, far):
         """ Scale specified depth range to uint and return as np.uint8 array """
         return scale_depth(self.depth(), near, far)
+
+
+def kinect_topics(device_type='kinect', camera_name='', quality=()):
+    '''Returns default rgb and depth topics for the kinect sensors
+
+    Keyword Arguments:
+        device_type {str} -- device type, one of 'kinect', 'kinect2', 'realsense' (default: {'kinect'})
+        camera_name {str} -- camera unique identifier (default: {''})
+        quality {tuple} -- rgb and depth quality, only for kinect2 (default: {()})
+
+    Raises:
+        RuntimeError -- for unknown device type
+
+    Returns:
+        tuple -- rgb and depth topics
+    '''
+
+    if device_type == 'kinect':
+        camera_name = camera_name or 'camera'
+        color_topic = '/{}/rgb/image_rect_color'.format(camera_name)
+        depth_topic = '/{}/depth_registered/image'.format(camera_name)
+
+    elif device_type == 'kinect2':
+        camera_name = camera_name or 'kinect2'
+        quality = quality or ('sd', 'sd')
+        color_topic = '/{}/{}/image_color_rect'.format(camera_name, quality[0])
+        depth_topic = '/{}/{}/image_depth_rect'.format(camera_name, quality[1])
+
+    elif device_type == 'realsense':
+        camera_name = camera_name or 'camera'
+        color_topic = '/{}/color/image_rect_color'.format(camera_name)
+        depth_topic = '/{}/depth/image_rect_raw'.format(camera_name)
+
+    else:
+        raise RuntimeError('Unknown RGBD device type: {}'.format(device_type))
+
+    return color_topic, depth_topic
