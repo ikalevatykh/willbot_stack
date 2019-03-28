f4ef49f768cd6038ae6b32c6e4870708f2133a15
Igor Kalevatykh <kalevatykh@gmail.com>
2019-03-28 12:50:40 +0100

    fix gripper initialization

--- willbot_bringup/script/init_gripper.py
+++ willbot_bringup/script/init_gripper.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python
 
+import sys
 import rospy
+from sensor_msgs.msg import JointState
 from std_srvs.srv import Trigger
 
-
-init_srv = rospy.ServiceProxy('/gripper/init', Trigger)
+rospy.myargv(argv=sys.argv)
+rospy.init_node('init_gripper', anonymous=False)
 
 try:
-    init_srv.wait_for_service(timeout=5.0)
+    gripper = rospy.get_param('~gripper_name', 'gripper')
+
+    # waiting for a state to be ensure the gripper is connected
+    topic = '/{}/joint_states'.format(gripper)
+    rospy.wait_for_message(topic, JointState, timeout=5.0)
+
+    # initialize the gripper if it is not yet initialized
+    service = '/{}/init'.format(gripper)
+    rospy.wait_for_service(service, timeout=5.0)
+
+    init_srv = rospy.ServiceProxy(service, Trigger)
     resp = init_srv()
+
     if not resp.success:
         raise RuntimeError(resp.message)
+
     rospy.loginfo('Gripper initialized')
 except Exception as e:
     rospy.logerr('Error during gripper initialization: {}'.format(e))
