394432837967ba67e98c44274ae8d92693f84c90
Igor Kalevatykh <kalevatykh@gmail.com>
2019-05-17 13:37:02 +0200

    joy control with a new interface

--- willbot_scripts/scripts/joy_control.py
+++ willbot_scripts/scripts/joy_control.py
@@ -2,34 +2,52 @@
 
 from __future__ import print_function
 
-import sys
-
-import moveit_commander
 import numpy as np
 import rospy
 
+from geometry_msgs.msg import TwistStamped, Vector3
+
+from willbot_utils.arm import UR5
+from willbot_utils.controller import Controller, ControllerManager
 from willbot_utils.joystick import Joystick
-from willbot_utils.scene import StandardScene
-from willbot_utils.velocity_controller import VelocityController, ControllerException
 from willbot_utils.hand import RobotiqHand
+from willbot_utils.scene import StandardScene
+
+
+class VelocityController(Controller):
+    def __init__(self, cm, name='tool_velocity_controller'):
+        Controller.__init__(self, cm, name)
+        topic = '/{}/tool_velocity_controller/command'.format(cm.group)
+        self._pub = rospy.Publisher(topic, TwistStamped, queue_size=10)
+
+    def command(self, linear, angular):
+        msg = TwistStamped()
+        msg.header.frame_id = 'world'
+        msg.twist.linear = Vector3(*linear)
+        msg.twist.angular = Vector3(*angular)
+        self._pub.publish(msg)
 
 
 class JoyControllerNode(object):
     def __init__(self):
         print('Setup...')
 
-        arm = moveit_commander.MoveGroupCommander("manipulator")
-        arm.set_planner_id("RRTConnectkConfigDefault")
-        arm.set_max_velocity_scaling_factor(0.5)
-        scene = StandardScene()
-        joy = Joystick()
         hand = RobotiqHand()
+        hand.mode = 1
+        hand.target_velocity = 15
+        hand.target_effort = 5
+
+        arm = UR5(hand=hand)
         ws = np.array([[0.3, -0.2, 0.05], [0.9, 0.2, 0.25]])
-        arm.set_workspace(ws.flatten())        
+        arm.set_workspace(ws.flatten())
+
+        scene = StandardScene(arm)
+        joy = Joystick()
 
         print('Ready!')
 
-        with VelocityController(arm, ws) as arm_ctrl:
+        cm = ControllerManager('arm')
+        with VelocityController(cm) as arm_ctrl:
             self.run(joy, arm_ctrl, hand)
 
     def run(self, joy, arm_ctrl, hand):
@@ -37,7 +55,7 @@ class JoyControllerNode(object):
         max_angular_vel = rospy.get_param('max_angular_vel', 0.1)
 
         hand.mode = 1
-        grasp = hand.is_object_held         
+        grasp = False
 
         rate = rospy.Rate(20)
         while not rospy.is_shutdown():
@@ -53,22 +71,27 @@ class JoyControllerNode(object):
                     # linear step
                     v = signal * [1, -1, 1] * max_angular_vel
                     w = (0, 0, 0)
-                try:
-                    arm_ctrl.step(0.1, v, w, check=True)
-                except ControllerException as e:
-                    print(e)
+
+                arm_ctrl.command(v, w)
 
             if np.any(joy.pushed[0:3]):
                 hand.mode = np.argmax(joy.pushed[0:3])
+                if grasp:
+                    hand.close(wait=False)
+                else:
+                    hand.open(wait=False)
                 hand.move(int(grasp) * 255)
                 print('Changed hand mode:', hand.mode)
 
             if np.any(joy.pushed[4]):
                 grasp = not grasp
-                hand.move(int(grasp) * 255)
+                if grasp:
+                    hand.close(wait=False)
+                else:
+                    hand.open(wait=False)
                 print('Grasped' if grasp else 'Released')
 
-            print(arm_ctrl._group.get_current_pose().pose)
+            # print(arm_ctrl._group.get_current_pose().pose)
 
             rate.sleep()
 
